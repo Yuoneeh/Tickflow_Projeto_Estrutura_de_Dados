7b8aa626afdb8fbdf05412cefc9fd80dca833607
GABRIEL PEREIRA <gabriel.jptj@gmail.com>
2026-05-13 15:38:09 -0300

    Adicionando arquivos python para o backend

---
+++ src/service/__init__.py
@@ -0,0 +1,5 @@
+from .evento_service import EventoService
+from .compra_service import CompraService
+from .auth_service import AuthService
+
+__all__ = ["EventoService", "CompraService", "AuthService"]
