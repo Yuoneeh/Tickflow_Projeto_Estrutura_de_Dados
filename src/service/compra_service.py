8e04a226cded22dc3765308b584bd9775835c2f9
Eduardo <eduardogabrielsc@gmail.com>
2026-05-17 14:48:13 -0300

    Começando o compra_service
    não sei mexer muito então fiz só a base, vou deixar pra quem sabe o que ta fazendo. 0_0

---
+++ src/service/compra_service.py
@@ -0,0 +1,21 @@
+"""
+Service: Compra Service
+
+Controla: 
+Fila de Compra (FIFO)
+Historico de Ações (LIFO Pilha)
+
+"""
+
+from collections import deque
+from datetime import datetime
+
+class CompraService :
+    def __init__(self, evento_service):
+        self.evento_service = evento_service
+
+        #Fila
+        self.fila = deque()
+
+        #Pilha
+        self.pilha = []
