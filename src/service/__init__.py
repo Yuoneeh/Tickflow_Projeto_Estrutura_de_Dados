c43237240c31b8dca6828bb73f8d535c11382ba8
Eduardo Braga <Dudufelipeeduard@gmail.com>
2026-05-18 11:26:30 -0300

    test: Sistemas ed Pilha e Fila

--- src/service/__init__.py
+++ src/service/__init__.py
@@ -1,5 +1,5 @@
-from .evento_service import EventoService
-from .compra_service import CompraService
-from .auth_service import AuthService
+#from .evento_service import EventoService
+#from .compra_service import CompraService
+#from .auth_service import AuthService
 
-__all__ = ["EventoService", "CompraService", "AuthService"]
+#__all__ = ["EventoService", "CompraService", "AuthService"]
